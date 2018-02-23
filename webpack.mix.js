29cb522fbfdfcefd3dfdaa1dfcf1904de95886e1
haohexin <2554470320@qq.com>
2018-02-23 09:04:31 +0000

    WYSIWYG Editor

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,8 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .copyDirectory('resources/assets/editor/js', 'public/js')
+    .copyDirectory('resources/assets/editor/css', 'public/css')
+;
