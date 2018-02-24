11aa74d2e9270ccb4434318ca77d0ef272537555
haohexin <2554470320@qq.com>
2018-02-24 06:53:38 +0000

    Keep track of user last visited time

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ class User extends Authenticatable
 
     use HasRoles;
     use Traits\ActiveUserHelper;
+    use Traits\LastActivedAtHelper;
     use Notifiable {
         notify as protected laravelNotify;
     }
@@ -82,7 +83,7 @@ class User extends Authenticatable
     public function setAvatarAttribute($path)
     {
         // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
-        if ( ! starts_with($path, 'http')) {
+        if (!starts_with($path, 'http')) {
 
             // 拼接完整的 URL
             $path = config('app.url') . "/uploads/images/avatars/$path";
